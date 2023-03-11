c12a6f2143f3ebfafa280b12e41c4a1c02c14da4
Binayak Basu <121999431+basu-binayak@users.noreply.github.com>
2023-03-11 13:19:37 +0530

    Update Query.sql

--- Problem 7/Query.sql
+++ Problem 7/Query.sql
@@ -1,8 +1,8 @@
 select name
 from salesperson
 where name not in (select name 
-				   from salesperson 
-				   inner join orders 
-				   on salesperson.sales_id = orders.sales_id
-				   where com_id = (select com_id from company 
-								   where name = 'RED'))
+		   from salesperson 
+		   inner join orders 
+		   on salesperson.sales_id = orders.sales_id 
+		   where com_id = (select com_id from company 
+				   where name = 'RED'))
