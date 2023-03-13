e6886e19ce1b54066aedfde70765802dfeffb06e
Binayak Basu <121999431+basu-binayak@users.noreply.github.com>
2023-03-13 18:49:30 +0530

    Update Query 3.sql

--- Problem 1/Query 3.sql
+++ Problem 1/Query 3.sql
@@ -1,8 +1,8 @@
 
 select title,rating 
-from (select title, dense_rank() over(partition by year order by rating desc) as r_rank, rating
-	  from (select title, rating, substring(title,length(title)-5) as year
-			from imdb) as t1) as t2
+from (select title, dense_rank() over(partition by year order by rating desc) as r_rank, rating 
+      from (select title, rating, substring(title,length(title)-5) as year 
+	    from imdb) as t1) as t2
 where r_rank = 1 or r_rank = 2
 	
 
